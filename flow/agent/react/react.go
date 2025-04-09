b6c87bf8f8ef014e7fe9ebef37f5cea2882f467f
张子晨 <384758226@qq.com>
2025-04-09 21:24:12 +0800

    init

---
+++ flow/agent/react/react.go
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2024 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cloudwego/eino-ext/components/model/ark"
+	"github.com/cloudwego/eino/callbacks"
+	"github.com/cloudwego/eino/components/tool"
+	"github.com/cloudwego/eino/compose"
+	"github.com/cloudwego/eino/flow/agent"
+	"github.com/cloudwego/eino/flow/agent/react"
+	"github.com/cloudwego/eino/schema"
+
+	"github.com/cloudwego/eino-examples/flow/agent/react/tools"
+	"github.com/cloudwego/eino-examples/internal/logs"
+)
+
+func main() {
+	arkAPIKey := os.Getenv("ARK_API_KEY")
+	arkModelName := os.Getenv("ARK_MODEL_NAME")
+
+	ctx := context.Background()
+	arkModel, err := ark.NewChatModel(ctx, &ark.ChatModelConfig{
+		APIKey: arkAPIKey,
+		Model:  arkModelName,
+	})
+	if err != nil {
+		logs.Errorf("failed to create chat model: %v", err)
+		return
+	}
+
+	// prepare tools
+	restaurantTool := tools.GetRestaurantTool() // 查询餐厅信息的工具
+	dishTool := tools.GetDishTool()             // 查询餐厅菜品信息的工具
+
+	// prepare persona (system prompt) (optional)
+	persona := `# Character:
+你是一个帮助用户推荐餐厅和菜品的助手，根据用户的需要，查询餐厅信息并推荐，查询餐厅的菜品并推荐。
+`
+
+	// replace tool call checker with a custom one: check all trunks until you get a tool call
+	// because some models(claude or doubao 1.5-pro 32k) do not return tool call in the first response
+	// uncomment the following code to enable it
+	/*toolCallChecker := func(ctx context.Context, sr *schema.StreamReader[*schema.Message]) (bool, error) {
+		defer sr.Close()
+		for {
+			msg, err := sr.Recv()
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					// finish
+					break
+				}
+
+				return false, err
+			}
+
+			if len(msg.ToolCalls) > 0 {
+				return true, nil
+			}
+		}
+		return false, nil
+	}*/
+
+	ragent, err := react.NewAgent(ctx, &react.AgentConfig{
+		Model: arkModel,
+		ToolsConfig: compose.ToolsNodeConfig{
+			Tools: []tool.BaseTool{restaurantTool, dishTool},
+		},
+		// StreamToolCallChecker: toolCallChecker, // uncomment it to replace the default tool call checker with custom one
+	})
+	if err != nil {
+		logs.Errorf("failed to create agent: %v", err)
+		return
+	}
+
+	// if you want ping/pong, use Generate
+	// msg, err := agent.Generate(ctx, []*schema.Message{
+	// 	{
+	// 		Role:    schema.User,
+	// 		Content: "我在北京，给我推荐一些菜，需要有口味辣一点的菜，至少推荐有 2 家餐厅",
+	// 	},
+	// }, react.WithCallbacks(&myCallback{}))
+	// if err != nil {
+	// 	log.Printf("failed to generate: %v\n", err)
+	// 	return
+	// }
+	// fmt.Println(msg.String())
+
+	sr, err := ragent.Stream(ctx, []*schema.Message{
+		{
+			Role:    schema.System,
+			Content: persona,
+		},
+		{
+			Role:    schema.User,
+			Content: "我在北京，给我推荐一些菜，需要有口味辣一点的菜，至少推荐有 2 家餐厅",
+		},
+	}, agent.WithComposeOptions(compose.WithCallbacks(&LoggerCallback{})))
+	if err != nil {
+		logs.Errorf("failed to stream: %v", err)
+		return
+	}
+
+	defer sr.Close() // remember to close the stream
+
+	logs.Infof("\n\n===== start streaming =====\n\n")
+
+	for {
+		msg, err := sr.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				// finish
+				break
+			}
+			// error
+			logs.Infof("failed to recv: %v", err)
+			return
+		}
+
+		// 打字机打印
+		logs.Tokenf("%v", msg.Content)
+	}
+
+	logs.Infof("\n\n===== finished =====\n")
+
+}
+
+type LoggerCallback struct {
+	callbacks.HandlerBuilder // 可以用 callbacks.HandlerBuilder 来辅助实现 callback
+}
+
+func (cb *LoggerCallback) OnStart(ctx context.Context, info *callbacks.RunInfo, input callbacks.CallbackInput) context.Context {
+	fmt.Println("==================")
+	inputStr, _ := json.MarshalIndent(input, "", "  ") // nolint: byted_s_returned_err_check
+	fmt.Printf("[OnStart] %s\n", string(inputStr))
+	return ctx
+}
+
+func (cb *LoggerCallback) OnEnd(ctx context.Context, info *callbacks.RunInfo, output callbacks.CallbackOutput) context.Context {
+	fmt.Println("=========[OnEnd]=========")
+	outputStr, _ := json.MarshalIndent(output, "", "  ") // nolint: byted_s_returned_err_check
+	fmt.Println(string(outputStr))
+	return ctx
+}
+
+func (cb *LoggerCallback) OnError(ctx context.Context, info *callbacks.RunInfo, err error) context.Context {
+	fmt.Println("=========[OnError]=========")
+	fmt.Println(err)
+	return ctx
+}
+
+func (cb *LoggerCallback) OnEndWithStreamOutput(ctx context.Context, info *callbacks.RunInfo,
+	output *schema.StreamReader[callbacks.CallbackOutput]) context.Context {
+
+	var graphInfoName = react.GraphName
+
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				fmt.Println("[OnEndStream] panic err:", err)
+			}
+		}()
+
+		defer output.Close() // remember to close the stream in defer
+
+		fmt.Println("=========[OnEndStream]=========")
+		for {
+			frame, err := output.Recv()
+			if errors.Is(err, io.EOF) {
+				// finish
+				break
+			}
+			if err != nil {
+				fmt.Printf("internal error: %s\n", err)
+				return
+			}
+
+			s, err := json.Marshal(frame)
+			if err != nil {
+				fmt.Printf("internal error: %s\n", err)
+				return
+			}
+
+			if info.Name == graphInfoName { // 仅打印 graph 的输出, 否则每个 stream 节点的输出都会打印一遍
+				fmt.Printf("%s: %s\n", info.Name, string(s))
+			}
+		}
+
+	}()
+	return ctx
+}
+
+func (cb *LoggerCallback) OnStartWithStreamInput(ctx context.Context, info *callbacks.RunInfo,
+	input *schema.StreamReader[callbacks.CallbackInput]) context.Context {
+	defer input.Close()
+	return ctx
+}
---
+++ go.mod
@@ -0,0 +1,76 @@
+module github.com/cloudwego/eino-examples
+
+go 1.23.4
+
+toolchain go1.23.6
+
+require (
+	github.com/bytedance/sonic v1.13.2
+	github.com/cloudwego/eino v0.3.18
+	github.com/cloudwego/eino-ext/components/document/parser/html v0.0.0-20250117061805-cd80d1780d76
+	github.com/cloudwego/eino-ext/components/document/parser/pdf v0.0.0-20250117061805-cd80d1780d76
+	github.com/cloudwego/eino-ext/components/model/ark v0.1.0
+	github.com/cloudwego/eino-ext/components/model/deepseek v0.0.0-20250221090944-e8ef7aabbe10
+	github.com/cloudwego/eino-ext/components/model/ollama v0.0.0-20250221090944-e8ef7aabbe10
+	github.com/cloudwego/eino-ext/components/model/openai v0.0.0-20250221090944-e8ef7aabbe10
+	github.com/cloudwego/eino-ext/components/retriever/volc_vikingdb v0.0.0-20250319082935-6219ec437e56
+	github.com/cloudwego/eino-ext/components/tool/duckduckgo v0.0.0-20250221090944-e8ef7aabbe10
+	github.com/cloudwego/eino-ext/devops v0.1.7
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc
+	github.com/getkin/kin-openapi v0.118.0
+	github.com/ollama/ollama v0.4.0
+	github.com/stretchr/testify v1.10.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/bytedance/sonic/loader v0.2.4 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cloudwego/base64x v0.1.5 // indirect
+	github.com/cloudwego/eino-ext/libs/acl/openai v0.0.0-20250221090944-e8ef7aabbe10 // indirect
+	github.com/cohesion-org/deepseek-go v1.2.3 // indirect
+	github.com/dslipak/pdf v0.0.2 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/goph/emperror v0.17.2 // indirect
+	github.com/gorilla/css v1.0.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/invopop/yaml v0.3.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/matoous/go-nanoid v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.27 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/nikolalohinski/gonja v1.5.3 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/perimeterx/marshmallow v1.1.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/sashabaranov/go-openai v1.37.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/slongfield/pyfmt v0.0.0-20220222012616-ea85ff4c361f // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/volcengine/volc-sdk-golang v1.0.196 // indirect
+	github.com/volcengine/volcengine-go-sdk v1.0.185 // indirect
+	github.com/yargevad/filepathx v1.0.0 // indirect
+	golang.org/x/arch v0.14.0 // indirect
+	golang.org/x/crypto v0.34.0 // indirect
+	golang.org/x/exp v0.0.0-20250218142911-aa4b98e5adaa // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
