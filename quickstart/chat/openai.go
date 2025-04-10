bf4496ec62d42b4e5f79b59e655f32b6cdace624
Zijin <2030110765@stmail.ntu.edu.cn>
2025-04-10 09:02:51 +0800

    add doubao api

--- quickstart/chat/openai.go
+++ quickstart/chat/openai.go
@@ -23,12 +23,18 @@ import (
 
 	"github.com/cloudwego/eino-ext/components/model/openai"
 	"github.com/cloudwego/eino/components/model"
+	"github.com/joho/godotenv"
 )
 
 func createOpenAIChatModel(ctx context.Context) model.ChatModel {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("加载.env文件失败:%v", err)
+	}
 	key := os.Getenv("OPENAI_API_KEY")
 	modelName := os.Getenv("OPENAI_MODEL_NAME")
 	baseURL := os.Getenv("OPENAI_BASE_URL")
+	log.Printf(baseURL)
 	chatModel, err := openai.NewChatModel(ctx, &openai.ChatModelConfig{
 		BaseURL: baseURL,
 		Model:   modelName,
